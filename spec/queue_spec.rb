7c33e8baae019c1d3676c907a123ef01cab99cba
cpearce31 <cpearce31@gmail.com>
2018-07-19 12:44:25 -0400

    Add RSpec files for stack and queue
    Same tests from the JS versions, just testing the Ruby implementations this
    time.

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -2,11 +2,39 @@
 
 require_relative '../lib/queue.rb'
 
-RSpec.describe Fifo do
-  context 'a new queue' do
+RSpec.describe 'Queue - Ruby' do
+  queue = Fifo.new
+
+  it 'has an `empty?` method' do
+    expect(queue).to respond_to(:empty?)
+  end
+
+  it 'is empty when newly created' do
+    expect(queue.empty?).to be true
+  end
+
+  it 'has a `enqueue` method' do
+    expect(queue).to respond_to(:enqueue)
+  end
+
+  it 'has a `dequeue` method' do
+    expect(queue).to respond_to(:dequeue)
+  end
+
+  it '`dequeue` method returns the first enqueued value' do
     queue = Fifo.new
-    it 'is empty' do
-      expect(queue.empty?).to eq(true)
-    end
+
+    queue.enqueue(40)
+    queue.enqueue(50)
+    expect(queue.dequeue).to eq(40)
+  end
+
+  it '`dequeue` method removes the first value from the store' do
+    queue = Fifo.new
+
+    queue.enqueue(40)
+    queue.enqueue(50)
+    queue.dequeue
+    expect(queue.dequeue).to eq(50)
   end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -2,11 +2,39 @@
 
 require_relative '../lib/stack.rb'
 
-RSpec.describe Stack do
-  context 'a new stack' do
+RSpec.describe 'Stack - Ruby' do
+  stack = Stack.new
+
+  it 'has an `empty?` method' do
+    expect(stack).to respond_to(:empty?)
+  end
+
+  it 'is empty when newly created' do
+    expect(stack.empty?).to be true
+  end
+
+  it 'has a `push` method' do
+    expect(stack).to respond_to(:push)
+  end
+
+  it 'has a `pop` method' do
+    expect(stack).to respond_to(:pop)
+  end
+
+  it '`pop` method returns the last pushed value' do
     stack = Stack.new
-    it 'is empty' do
-      expect(stack.empty?).to eq(true)
-    end
+
+    stack.push(40)
+    stack.push(50)
+    expect(stack.pop).to eq(50)
+  end
+
+  it '`pop` method removes the last value from the store' do
+    stack = Stack.new
+
+    stack.push(40)
+    stack.push(50)
+    stack.pop
+    expect(stack.pop).to eq(40)
   end
 end
