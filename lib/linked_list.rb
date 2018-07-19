3644b7f083d40d4e9874b1aabd190def3d9fa325
cpearce31 <cpearce31@gmail.com>
2018-07-19 12:54:11 -0400

    Fix up starter code
    To match tests and README.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,43 @@
 
 # A LinkedList class
 class LinkedList
+  attr_accesor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def search(value)
+  end
+
+  def last
+  end
+
+  def length
+  end
+
+  def insert_after(target, value)
+  end
+
+  def remove(target)
+  end
+
+  def to_s
+  end
+end
+
+# a class resprenting a single "link" or node in a linked list
+class Node
+  attr_accessor :value, next_node
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
 end
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-# A queue class
+# A queue class, called `Fifo` because `Queue` already exists in Ruby
 class Fifo
 end
